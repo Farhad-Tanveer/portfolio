14db824a09bac3b6781802844943c1a07612c864
Farhad-Tanveee <farhadtanveer05u@example.com>
2023-07-03 13:41:27 +0200

    added projects section

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -3,67 +3,178 @@ import { useState } from "react";
 // import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 // import { BsArrowUpRight } from "react-icons/bs";
+import { FaGithub, FaLine, FaLink } from "react-icons/fa";
 import img1 from "../../assets/projects/chef-recipe-hunter.png";
 import img2 from "../../assets/projects/summer-camp.png";
-import img3 from "../../assets/projects/dragon-news.png";
+import img3 from "../../assets/projects/career-hub.png";
 
 const Works = () => {
   const [projects, setProjects] = useState([]);
 
   return (
-    <div name="works" className=" w-full md:h-screen text-[#35404E]">
+    <div name="works" className=" w-full py-20  text-[#35404E]">
       <div className=" max-w-[1200px] mx-auto p-4">
-        <div className=" grid lg:grid-cols-2 pt-20">
+        <div className=" grid lg:grid-cols-2">
           <h1 className=" text-5xl font-poppins font-bold">
             Look at my <br /> recent projects{" "}
           </h1>
-          {/* <div>
-            <Tabs>
-              <TabList>
-                <div className="flex justify-center items-center gap-6 mt-10">
-                  <Tab>
-                    <button className="btn btn-accent rounded-full">
-                      All <BsArrowUpRight />
-                    </button>{" "}
-                  </Tab>
-                  <Tab>
-                    <button className="btn btn-accent rounded-full">
-                      Basic <BsArrowUpRight />
-                    </button>
-                  </Tab>
-                  <Tab>
-                    <button className="btn btn-accent rounded-full">
-                      MERN <BsArrowUpRight />
-                    </button>
-                  </Tab>
-                </div>
-              </TabList>
-
-              <TabPanel>
-                <h2>Any content 1</h2>
-              </TabPanel>
-              <TabPanel>
-                <h2>Any content 2</h2>
-              </TabPanel>
-            </Tabs>
-          </div> */}
         </div>
         <div>
+          {/* first project */}
           <div className="card lg:card-side bg-base-100 mt-20">
-            <figure className=" w-[600px] h-[400px]">
-              <div className="rounded-xl overflow-hidden">
+            <div className="relative max-w-full h-80 overflow-hidden rounded-lg">
+              <div className="h-max w-[600px] transition-transform duration-[5000ms] ease-linear transform translate-y-0 hover:-translate-y-[calc(100%-18rem)] rounded-lg">
                 <img
-                  className="object-top hover:object-bottom duration-700 "
                   src={img1}
-                  alt="Album"
+                  alt=""
+                  className="w-full object-cover object-top rounded-lg"
                 />
               </div>
-            </figure>
-            <div className="card-body ml-32">
-              <h2 className="card-title">New album is released!</h2>
-              <p>Click the button to listen on Spotiwhy app.</p>
-              <div className="card-actions justify-start">
-                <button className="btn btn-primary">Listen</button>
+            </div>
+            <div className="card-body lg:w-1/2 ml-24">
+              <h2 className="card-title">Foodism</h2>
+              <p>
+                Foodism is a website dedicated to showcasing Chinese recipes. It
+                serves as a platform for renowned Chinese chefs to share their
+                culinary expertise and authentic Chinese dishes.{" "}
+              </p>
+              <div className="flex gap-3 mb-7">
+                <button className=" btn btn-sm normal-case">JavaScript</button>
+                <button className=" btn btn-sm normal-case">ReactJs</button>
+                <button className=" btn btn-sm normal-case">MongoDB</button>
+                <button className=" btn btn-sm normal-case">ExpressJs</button>
+                <button className=" btn btn-sm normal-case">NodeJs</button>
+              </div>
+              <div className="flex gap-3">
+                <button className=" btn normal-case">
+                  <a
+                    href="https://github.com/Farhad-Tanveer/chef-recipe-hunter-client"
+                    target="_blank"
+                  >
+                    Client
+                  </a>
+                  <FaGithub></FaGithub>
+                </button>
+                <button className=" btn normal-case">
+                  <a
+                    href="https://github.com/Farhad-Tanveer/chef-recipe-hunter-server"
+                    target="_blank"
+                  >
+                    Server
+                  </a>
+                  <FaGithub></FaGithub>
+                </button>
+                <button className=" btn normal-case">
+                  <a
+                    href="https://chef-recipe-hunter-c1208.web.app/"
+                    target="_blank"
+                  >
+                    Live Site
+                  </a>
+                  <FaLink></FaLink>
+                </button>
+              </div>
+            </div>
+          </div>
+
+          {/* second project */}
+          <div className="card lg:card-side bg-base-100 mt-20">
+            <div className="card-body pl-0 lg:w-1/2">
+              <h2 className="card-title">Sporty Summer</h2>
+              <p>
+                This is a sports academy website project designed for a summer
+                camp school. The website aims to provide a platform for students
+                to enroll and learn various sports activities.{" "}
+              </p>
+              <div className="flex gap-3 mb-7">
+                <button className=" btn btn-sm normal-case">JavaScript</button>
+                <button className=" btn btn-sm normal-case">ReactJs</button>
+                <button className=" btn btn-sm normal-case">MongoDB</button>
+                <button className=" btn btn-sm normal-case">ExpressJs</button>
+                <button className=" btn btn-sm normal-case">NodeJs</button>
+              </div>
+              <div className="flex gap-3">
+                <button className=" btn normal-case">
+                  <a
+                    href="https://github.com/Farhad-Tanveer/summer-camp-client"
+                    target="_blank"
+                  >
+                    Client
+                  </a>
+                  <FaGithub></FaGithub>
+                </button>
+                <button className=" btn normal-case">
+                  <a
+                    href="https://github.com/Farhad-Tanveer/summer-camp-server"
+                    target="_blank"
+                  >
+                    Server
+                  </a>
+                  <FaGithub></FaGithub>
+                </button>
+                <button className=" btn normal-case">
+                  <a href="https://summer-camp-b9765.web.app/" target="_blank">
+                    Live Site
+                  </a>
+                  <FaLink></FaLink>
+                </button>
+              </div>
+            </div>
+            <div className="relative max-w-full h-80 overflow-hidden rounded-lg">
+              <div className="h-max w-[600px] transition-transform duration-[5000ms] ease-linear transform translate-y-0 hover:-translate-y-[calc(100%-18rem)] rounded-lg">
+                <img
+                  src={img2}
+                  alt=""
+                  className="w-full object-cover object-top rounded-lg"
+                />
+              </div>
+            </div>
+          </div>
+
+          {/* third project */}
+          <div className="card lg:card-side bg-base-100 mt-20">
+            <div className="relative max-w-full h-80 overflow-hidden rounded-lg">
+              <div className="h-max w-[600px] transition-transform duration-[5000ms] ease-linear transform translate-y-0 hover:-translate-y-[calc(100%-18rem)] rounded-lg">
+                <img
+                  src={img3}
+                  alt=""
+                  className="w-full object-cover object-top rounded-lg"
+                />
+              </div>
+            </div>
+            <div className="card-body lg:w-1/2 ml-24">
+              <h2 className="card-title">Dream Career</h2>
+              <p>
+                Foodism is a website dedicated to showcasing Chinese recipes. It
+                serves as a platform for renowned Chinese chefs to share their
+                culinary expertise and authentic Chinese dishes.{" "}
+              </p>
+              <div className="flex gap-3 mb-7">
+                <button className=" btn btn-sm normal-case">JavaScript</button>
+                <button className=" btn btn-sm normal-case">ReactJs</button>
+                <button className=" btn btn-sm normal-case">Tailwind</button>
+                <button className=" btn btn-sm normal-case">HTML5</button>
+                <button className=" btn btn-sm normal-case">CSS</button>
+              </div>
+              <div className="flex gap-3">
+                <button className=" btn normal-case">
+                  <a
+                    href="https://github.com/Farhad-Tanveer/career-hub"
+                    target="_blank"
+                  >
+                    Client
+                  </a>
+                  <FaGithub></FaGithub>
+                </button>
+                <button className=" btn normal-case">
+                  <a
+                    href="https://effulgent-rabanadas-271b23.netlify.app/"
+                    target="_blank"
+                  >
+                    Live Site
+                  </a>
+                  <FaLink></FaLink>
+                </button>
               </div>
             </div>
           </div>
